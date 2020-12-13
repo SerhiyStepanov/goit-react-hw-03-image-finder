fb3eb4e27ea25fc5a38b0ecaffb0fb290630260e
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-12-13 19:55:28 +0200

    add loadded

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+// import Button from "../Button/Button";
 import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loaded from "../Loader";
 import s from "./ImageGallery.module.css";
 
 export default class ImageGallery extends Component {
@@ -9,15 +9,37 @@ export default class ImageGallery extends Component {
     search: null,
     error: null,
     status: "idle",
+    page: 1,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const prevSearh = prevProps.search;
     const currentSearch = this.props.search;
+
+    const prevPage = prevProps.page;
+    const currentPage = this.props.page;
+
+    if (prevPage !== currentPage) {
+      this.setState(({ page }) => ({
+        page: page + 1,
+      }));
+      fetch(
+        `https://pixabay.com/api/?key=8315600-a916a243d8ea2edafddc43bfd&q=${currentSearch}&image_type=photo&orientation=horizontal&page=${currentPage}&per_page=12`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) =>
+          this.setState(({ search }) => ({
+            search: [...search, ...data.hits],
+          }))
+        );
+    }
+
     if (prevSearh !== currentSearch) {
       this.setState({ status: "pending" });
       fetch(
-        `https://pixabay.com/api/?key=8315600-a916a243d8ea2edafddc43bfd&q=${currentSearch}&image_type=photo&orientation=horizontal&page=1&per_page=12`
+        `https://pixabay.com/api/?key=8315600-a916a243d8ea2edafddc43bfd&q=${currentSearch}&image_type=photo&orientation=horizontal&page=${currentPage}&per_page=12`
       )
         .then((response) => {
           if (response.ok) {
@@ -29,7 +51,10 @@ export default class ImageGallery extends Component {
           );
         })
         .then((data) =>
-          this.setState({ search: data.hits, status: "resolved" })
+          this.setState({
+            search: data.hits,
+            status: "resolved",
+          })
         )
         .catch((error) => this.setState({ error, status: "rejected" }));
     }
@@ -39,19 +64,11 @@ export default class ImageGallery extends Component {
     const { search, error, status } = this.state;
 
     if (status === "idle") {
-      return <h2>stend</h2>;
+      return <h2 style={{ textAlign: "center" }}>Введите параметы поиска</h2>;
     }
 
     if (status === "pending") {
-      return (
-        <Loader
-          style={{ textAlign: "center" }}
-          type="ThreeDots"
-          color="#3f51b5"
-          height={80}
-          width={80}
-        />
-      );
+      return <Loaded />;
     }
 
     if (status === "rejected") {
@@ -61,10 +78,9 @@ export default class ImageGallery extends Component {
     if (status === "resolved") {
       return (
         <ul className={s.ImageGallery}>
-          {search &&
-            search.map((el) => (
-              <ImageGalleryItem key={el.id} webformatURL={el.webformatURL} />
-            ))}
+          {search.map((el) => (
+            <ImageGalleryItem key={el.id} webformatURL={el.webformatURL} />
+          ))}
         </ul>
       );
     }
--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,3 +1,17 @@
+// import { Component } from "react";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-
-
+export default function Loaded() {
+  return (
+    <div>
+      <Loader
+        style={{ textAlign: "center" }}
+        type="ThreeDots"
+        color="#3f51b5"
+        height={100}
+        width={100}
+      />
+    </div>
+  );
+}
